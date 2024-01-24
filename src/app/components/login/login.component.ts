19853c2bc0f56a170693e634d2d4587a7ea31f25
MiguelOrtsB <miguelob1997@gmail.com>
2024-01-24 10:48:29 +0100

    new commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {Usuario} from "../../models/usuario";
+import {LoginService} from "../../services/login.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  // @ts-ignore
+  posts = new Usuario('', '');
+
+  /* Creamos una instancia (objeto) de la clase Service */
+  constructor(private service:LoginService) {}
+
+  emailUsuario: string = "";
+  contra: string = "";
+
+  onClick(){
+   const jsonData: string = JSON.stringify({
+     email: this.emailUsuario,
+     password: this.contra
+   })
+     console.log(jsonData);
+
+  }
 
 }
