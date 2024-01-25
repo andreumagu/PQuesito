7cef33d9e544f6fbde0e53bc3f4ed2b377cff9e1
MiguelOrtsB <miguelob1997@gmail.com>
2024-01-25 08:45:34 +0100

    error solucionado al importar 'provideHttpClient' en el archivo app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
+import { HttpClient} from "@angular/common/http";
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
