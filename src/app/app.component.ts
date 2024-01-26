82a1bf9204a127b1538828654b83117f0cfe4246
Marc Morlá Isern <marc.morla20@gmail.com>
2024-01-26 18:09:54 +0100

    Maquetación homePage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {CommonModule} from "@angular/common";
+import {HomeComponent} from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {Usuario} from "../../models/usuario";
 import {LoginService} from "../../services/login.service";
 import {FormsModule} from "@angular/forms";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgOptimizedImage
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
