34a2e1025d3d4873c4126c568c5ffc14736538c3
Marc Morlá Isern <marc.morla20@gmail.com>
2024-01-30 10:48:39 +0100

    Página de modulos casi terminada, hay que implementar el gráfico y las tablas pertinentes. También hay que retocar y centrar nombres.

--- src/app/components/modulos/modulos.component.ts
+++ src/app/components/modulos/modulos.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-modulos',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatButtonModule, MatSelectModule, MatIconModule, MatDividerModule],
   templateUrl: './modulos.component.html',
   styleUrl: './modulos.component.css'
 })
 export class ModulosComponent {
-
+  showFiller = false;
 }
