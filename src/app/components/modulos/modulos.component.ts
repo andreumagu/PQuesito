59945426ae103095353a54b06fec5b9eaadd3f7e
andreumagu <andreumagu@gmail.com>
2024-02-05 08:23:16 +0100

    Add user data

--- src/app/components/modulos/modulos.component.ts
+++ src/app/components/modulos/modulos.component.ts
@@ -5,8 +5,13 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import {LoginService} from "../../services/login.service";
+import {DecodedToken} from "../../interfaces/decoded-token";
+import {jwtDecode} from "jwt-decode";
+import {Usuario} from "../../models/usuario";
+import {DatosService} from "../../services/datos.service";
+import {ComprobartokenService} from "../../services/comprobartoken.service";
 import {Router} from "@angular/router";
+import {LoginService} from "../../services/login.service";
 
 
 @Component({
@@ -19,7 +24,35 @@ import {Router} from "@angular/router";
 export class ModulosComponent {
   showFiller = false;
 
-  constructor(private router: Router) {}
+
+  usuario = new Usuario("", "", "", "", "", "", "");
+
+  constructor(private comprobar: ComprobartokenService, private router: Router) {}
+
+  ngOnInit(){
+    // Recuperar el el token
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const decodedToken: DecodedToken = jwtDecode(token);
+
+      // Acceder a la información de data
+      const data = decodedToken.data;
+
+      // Asignar propiedades del usuario
+      this.usuario.dni = data.dni;
+      this.usuario.nombre = data.nombre;
+      this.usuario.apellido1 = data.apellido1;
+      this.usuario.apellido2 = data.apellido2;
+      this.usuario.email = data.Email;
+      this.usuario.curso = "1r";
+      this.usuario.ciclo = "Desarrollo de Aplicaciones Web";
+
+    }else {
+      this.router.navigate(['/login']);
+    }
+  }
+
   onClickHome (){
     this.router.navigate(['/home']);
   }
