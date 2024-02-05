a0a360d47deb62f94d8fade100717c3f1505f808
MiguelOrtsB <miguelob1997@gmail.com>
2024-02-05 08:05:36 +0100

    nuevas modificaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
 import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 import {ModulosComponent} from "./components/modulos/modulos.component";
+import {AuthGuard} from "./auth.guard";
+import {ModuleGuard} from "./module.guard";
+
+
 
 
 export const routes: Routes = [
   {path:'', redirectTo:'/login', pathMatch:'full'},
-  {path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   {path: 'login', component: LoginComponent},
-  {path: 'modulos', component: ModulosComponent},
+  { path: 'modulos', component: ModulosComponent, canActivate: [ModuleGuard] },
   {path:'**', component:PageNotFoundComponent}
 ];
---
+++ src/app/module.guard.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { CanActivateFn } from '@angular/router';
+
+// @ts-ignore
+import { moduleGuard } from './module.guard';
+
+describe('moduleGuard', () => {
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+      TestBed.runInInjectionContext(() => moduleGuard(...guardParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeGuard).toBeTruthy();
+  });
+});
